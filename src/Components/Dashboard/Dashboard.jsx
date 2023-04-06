0376b11afa68e05ef6fc1fc2e17d14c09966a94a
Shivay2243 <nagarajanlap@gmail.com>
2023-04-06 14:21:30 +0530

    Changes to add donor detail in request data in Server index.js

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -84,6 +84,7 @@ const Dashboard = () => {
                             setIsloading(false)
                         }
                     })
+                    
             }
         })
     },[])
